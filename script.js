53c1560e5db5ef0d10c247225607c5fa3201d64d
Aayush Mishra <aayushmishra1105@gmail.com>
2024-08-12 03:25:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Engineer", "Programmer", "Avid Astronomer", "Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
