7dbfea7b29038aee5e4c4113de0e30572cfed04c
Aayush Mishra <aayushmishra1105@gmail.com>
2024-08-12 06:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Mechatronics Engineer", "Programmer", "Avid Astronomer", "Student Teacher", "Freelancer"],
+        strings: [" Mechatronics Engineer ", " Programmer ", " Avid Astronomer ", " Student Teacher ", " Freelancer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Mechatronics Engineer", "Programmer", "Avid Astronomer", "Student Teacher", "Freelancer"],
+        strings: [" Mechatronics Engineer ", " Programmer ", " Avid Astronomer ", " Student Teacher ", " Freelancer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
